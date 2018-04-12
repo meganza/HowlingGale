c13fc3aa09b9fcc747e4505a6d31623bfe78a6a7
Margaret Avancena <meganza@gmail.com>
2018-04-12 13:20:12 -0700

    Added nav scroll. To do: contact, video for banner, decorative nav fx

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
 console.log('hello');
+
+$(document).ready(function(){
+    $('#nav-services').click(function() {
+        $('html, body').animate({
+            scrollTop: $('#services').offset().top
+        }, 2000);
+    });
+
+    $('#nav-work').click(function() {
+        $('html, body').animate({
+            scrollTop: $('#work').offset().top
+        }, 2000);
+    });
+
+    $('#nav-team').click(function() {
+        $('html, body').animate({
+            scrollTop: $('#team').offset().top
+        }, 2000);
+    });
+
+    $('#nav-contact').click(function() {
+        $('html, body').animate({
+            scrollTop: $('#contact').offset().top
+        }, 2000);
+    });
+});
